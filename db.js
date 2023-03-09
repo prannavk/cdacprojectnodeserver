5e687fd55395eebeb08cfbfa8987fae7833d6935
w3_kpranav_70088 <krishnapk808@gmail.com>
2023-03-09 20:04:44 +0530

    node server running on local server successfully

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     connectionLimit: 20,
     user: "admin",
     password: "Amex618753",
-    host: "http://databaseamazonb.cj6dgqkguxl3.us-east-2.rds.amazonaws.com/",
+    host: "databaseamazonb.cj6dgqkguxl3.us-east-2.rds.amazonaws.com",
     port: 3306,
     database: "myamazon",
     timezone: 'utc'
